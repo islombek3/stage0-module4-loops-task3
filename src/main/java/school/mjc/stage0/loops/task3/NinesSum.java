fad443e092ea9d169dcbbd30a9e56f0572e55b8c
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:46:31 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        if (lengthOfLastNumber < 0){ 
+            System.out.println(0); 
+        } else {
+            String nine = "9"; //min
+            String val = nine.repeat(lengthOfLastNumber); //max
+            int sum = 0;
+            for (int i = val.length(); i >= nine.length(); i--){
+                String s = nine.repeat(i);
+                int a = Integer.parseInt(s);
+                sum += a;
+            }
+            System.out.println(sum);
+        }
     }
 }
