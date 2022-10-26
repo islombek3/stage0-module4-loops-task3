dc19efe1edf9fd547a1769d58d9690cdab11d67a
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:44:57 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        if(t < 0){t *= -1;}
+        String str = "" + t;
+        int sum = 0; 
+        for (int i = 0; i < str.length(); i++){
+            char chr = str.charAt(i);
+            int digit = Character.getNumericValue(chr);
+            sum += digit;
+        }
+        printSum(sum);
+    }
+    public void printSum(int sum){
+        System.out.println(sum);
     }
 }
