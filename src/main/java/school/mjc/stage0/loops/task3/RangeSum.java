60d530d261aa0dd39cf2384179d626c950e3c0e6
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:47:00 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int i = 0, sum = 0;
+        for (i = firstBoarder; i < secondBoarder + 1; i++){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
