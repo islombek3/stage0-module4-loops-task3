5c7d185a0c97f773b00fcd81b685570b6f253acb
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:45:27 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int val = 0;
+        int[] fib = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+        for (int i = 0; i < lastFibonacci; i++){
+            val = fib[i];
+            System.out.println(val);
+        } 
     }
 }
