1fa3e016d340912cff3212cddd2c4f6511a1c826
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:42:21 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        String abc = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < abc.length(); i++){
+            char c = abc.charAt(i);
+            System.out.println(c);
+        }
     }
 }
