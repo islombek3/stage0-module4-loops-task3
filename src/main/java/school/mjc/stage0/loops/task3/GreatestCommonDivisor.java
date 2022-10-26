c5510c241c67373da72647ad7a8319112461a384
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-26 12:45:59 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        for (int i = 100; i > 2; i--){
+            if (first % i == 0 && second % i == 0){
+                int gcd = i;
+                System.out.println(gcd);
+                break;
+            }
+        }
     }
 }
